c7b352fd7f8a741b4b3ce52ca0e282dbbfb3e469
Naomi229 <48450265+Naomi229@users.noreply.github.com>
2019-03-25 18:08:01 +0200

    inital version of code

---
+++ Premiu.java
@@ -0,0 +1,19 @@
+public class Premiu {
+    String nume;
+    Integer an;
+
+    //constructor
+    public Premiu(String nume, Integer an) {
+        this.nume = nume;
+        this.an = an;
+    }
+
+    //get-ers
+    public String getNume() {
+        return nume;
+    }
+
+    public Integer getAn() {
+        return an;
+    }
+}
